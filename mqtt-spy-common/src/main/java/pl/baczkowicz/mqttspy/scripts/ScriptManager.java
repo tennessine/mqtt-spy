228109e9bcbbffeb09848969c487b338edcc0a85
Kamil Baczkowicz <kamil.baczkowicz@gmail.com>
2014-12-09 23:28:33 +0000

    Complete javadoc

--- mqtt-spy-common/src/main/java/pl/baczkowicz/mqttspy/scripts/ScriptManager.java
+++ mqtt-spy-common/src/main/java/pl/baczkowicz/mqttspy/scripts/ScriptManager.java
@@ -35,23 +35,39 @@ import pl.baczkowicz.mqttspy.exceptions.CriticalException;
 import pl.baczkowicz.mqttspy.messages.IMqttMessage;
 import pl.baczkowicz.mqttspy.scripts.io.ScriptIO;
 
-// TODO: describe the class
+/**
+ * This class manages script creation and execution.
+ */
 public class ScriptManager
 {
+	/** Name of the variable in JS for received messages. */
 	public static final String RECEIVED_MESSAGE_PARAMETER = "receivedMessage";
 	
+	/** Name of the variable in JS for published/searched message. */
 	public static final String MESSAGE_PARAMETER = "message";
 	
+	/** Diagnostic logger. */
 	private final static Logger logger = LoggerFactory.getLogger(ScriptManager.class);
 	
+	/** Mapping between script files and scripts. */
 	private Map<File, Script> scripts = new HashMap<File, Script>();
 	
+	/** Used for notifying events related to script execution. */
 	private IScriptEventManager eventManager;
 
+	/** Executor for tasks. */
 	private Executor executor;
 
+	/** Connection for which the script will be run. */
 	protected IMqttConnection connection;
 	
+	/**
+	 * Creates the script manager.
+	 * 
+	 * @param eventManager The event manager to be used
+	 * @param executor The executor to be used
+	 * @param connection The connection for which to run the scripts
+	 */
 	public ScriptManager(final IScriptEventManager eventManager, final Executor executor, final IMqttConnection connection)
 	{
 		this.eventManager = eventManager;
@@ -59,11 +75,23 @@ public class ScriptManager
 		this.connection = connection;
 	}
 	
+	/**
+	 * Gets the file (script) name for the given file object.
+	 * 
+	 * @param file The file from which to get the filename
+	 * 
+	 * @return The name of the script file
+	 */
 	public static String getScriptName(final File file)
 	{
 		return file.getName().replace(".js",  "");
 	}
 	
+	/**
+	 * Creates and records a script with the given details.
+	 * 
+	 * @param scriptDetails The script details
+	 */
 	public void addScript(final ScriptDetails scriptDetails)
 	{
 		final File scriptFile = new File(scriptDetails.getFile());
@@ -78,50 +106,49 @@ public class ScriptManager
 		scripts.put(scriptFile, script);
 	}
 	
-	public void addScript(final String filename)
+	/**
+	 * Creates and records scripts with the given details.
+	 * 
+	 * @param scriptDetails The script details
+	 */
+	public void addScripts(final List<ScriptDetails> scriptDetails)
 	{
-		addScript(new ScriptDetails(false, filename));
-	}
-	
-	public void addScripts(final List<ScriptDetails> scriptFiles)
-	{
-		for (final ScriptDetails script : scriptFiles)
+		for (final ScriptDetails script : scriptDetails)
 		{
 			addScript(script);
 		}
 	}
-	
-	public Map<File, Script> getScripts()
-	{
-		return scripts;
-	}
 		
-	public void createScript(final Script scriptProperties,
+	/**
+	 * Populates the script object with the necessary values and references.
+	 * 
+	 * @param script The script object to be populated
+	 * @param scriptName The name of the script
+	 * @param scriptFile The script's file name 
+	 * @param connection The connection for which this script will be running
+	 * @param scriptDetails Script details
+	 */
+	public void createScript(final Script script,
 			String scriptName, File scriptFile, final IMqttConnection connection, final ScriptDetails scriptDetails)
-	// public PublicationScriptProperties createScript(String scriptName, File scriptFile, final MqttConnectionInterface connection, final Script scriptDetails)
 	{
 		final ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName("nashorn");										
 		
 		if (scriptEngine != null)
 		{
-//			final PublicationScriptProperties script = new PublicationScriptProperties(scriptName, scriptFile, 
-//					ScriptRunningState.NOT_STARTED, null, 0, scriptEngine, scriptDetails.isRepeat());
-			scriptProperties.setName(scriptName);
-			scriptProperties.setScriptFile(scriptFile);
-			scriptProperties.setStatus(ScriptRunningState.NOT_STARTED);
-			scriptProperties.setScriptEngine(scriptEngine);
-			scriptProperties.setScriptDetails(scriptDetails);
+			script.setName(scriptName);
+			script.setScriptFile(scriptFile);
+			script.setStatus(ScriptRunningState.NOT_STARTED);
+			script.setScriptEngine(scriptEngine);
+			script.setScriptDetails(scriptDetails);
 			
-			scriptProperties.setPublicationScriptIO(new ScriptIO(connection, eventManager, scriptProperties, executor));
+			script.setPublicationScriptIO(new ScriptIO(connection, eventManager, script, executor));
 			
 			final Map<String, Object> scriptVariables = new HashMap<String, Object>();
-			scriptVariables.put("mqttspy", scriptProperties.getPublicationScriptIO());	
+			scriptVariables.put("mqttspy", script.getPublicationScriptIO());	
 			scriptVariables.put("logger", LoggerFactory.getLogger(ScriptRunner.class));
-			scriptVariables.put("messageLog", scriptProperties.getPublicationScriptIO().getMessageLog());
+			scriptVariables.put("messageLog", script.getPublicationScriptIO().getMessageLog());
 			
 			putJavaVariablesIntoEngine(scriptEngine, scriptVariables);
-			
-//			return script;
 		}
 		else
 		{
@@ -129,7 +156,13 @@ public class ScriptManager
 					+ "Please either disable scripts in your configuration file or install the appropriate JRE/JDK.");
 		}
 	}
-					
+				
+	/**
+	 * Puts a the given map of variables into the engine.
+	 * 
+	 * @param engine The engine to be populated with variables
+	 * @param variables The variables to be populated
+	 */
 	public static void putJavaVariablesIntoEngine(final ScriptEngine engine, final Map<String, Object> variables)
 	{
 		final Bindings bindings = new SimpleBindings();
@@ -142,12 +175,13 @@ public class ScriptManager
 		engine.setBindings(bindings, ScriptContext.ENGINE_SCOPE);
 	}
 	
-	public void runScriptFile(final Script script)
-	{
-		runScriptFile(script, true);
-	}
-	
-	public void runScriptFile(final Script script, final boolean asynchronous)
+	/**
+	 * Runs the given script in a synchronous or asynchronous way.
+	 * 
+	 * @param script The script to run
+	 * @param asynchronous Whether to run the script asynchronously or not
+	 */
+	public void runScript(final Script script, final boolean asynchronous)
 	{
 		// Only start if not running already
 		if (!ScriptRunningState.RUNNING.equals(script.getStatus()))
@@ -164,30 +198,20 @@ public class ScriptManager
 			}
 		}
 	}	
-
-	public void runScriptFile(final File scriptFile)
-	{
-		final Script script = getScript(scriptFile); 
-		
-		if (script != null)
-		{
-			runScriptFile(script);
-		}
-		else
-		{
-			// Not good in this happens... ;-)
-			logger.warn("No script for {}", scriptFile.getName());
-		}
-	}
 	
-	public void runScriptFileWithReceivedMessage(final String scriptFile, final boolean asynchronous, final IMqttMessage receivedMessage)
+	/**
+	 * Runs the given script and passes the given message as a parameter. Defaults to the 'receivedMessage' parameter and synchronous execution.
+	 * 
+	 * @param script The script to run
+	 * @param message The message to be passed onto the script
+	 */
+	public void runScriptFileWithReceivedMessage(final String scriptFile, final IMqttMessage receivedMessage)
 	{
-		final Script script = getScript(new File(scriptFile));
+		final Script script = getScriptForFile(new File(scriptFile));
 		
 		if (script != null)
 		{
-			script.getScriptEngine().put(ScriptManager.RECEIVED_MESSAGE_PARAMETER, receivedMessage);
-			runScriptFile(script, asynchronous);
+			runScriptFileWithMessage(script, ScriptManager.RECEIVED_MESSAGE_PARAMETER, receivedMessage, false);
 		}
 		else
 		{
@@ -195,22 +219,48 @@ public class ScriptManager
 		}
 	}
 	
+	/**
+	 * Runs the given script and passes the given message as a parameter. Defaults to the 'message' parameter and asynchronous execution.
+	 * 
+	 * @param script The script to run
+	 * @param message The message to be passed onto the script
+	 */
 	public void runScriptFileWithMessage(final Script script, final IMqttMessage message)
 	{				
-		script.getScriptEngine().put(ScriptManager.MESSAGE_PARAMETER, message);
-		runScriptFile(script);		
+		runScriptFileWithMessage(script, ScriptManager.MESSAGE_PARAMETER, message, true);
 	}
 	
-	public Script getScript(final File scriptFile)
+	/**
+	 * Runs the given script and passes the given message as a parameter.
+	 * 
+	 * @param script The script to run
+	 * @param parameterName The name of the message parameter
+	 * @param message The message to be passed onto the script
+	 * @param asynchronous Whether the call should be asynchronous
+	 */
+	public void runScriptFileWithMessage(final Script script, final String parameterName, final IMqttMessage message, final boolean asynchronous)
+	{				
+		script.getScriptEngine().put(parameterName, message);
+		runScript(script, asynchronous);		
+	}
+	
+	/**
+	 * Gets script object for the given file.
+	 * 
+	 * @param scriptFile The file for which to get the script object
+	 * 
+	 * @return Script object or null if not found
+	 */
+	public Script getScriptForFile(final File scriptFile)
 	{
 		return scripts.get(scriptFile);
 	}
 
-	public IScriptEventManager getEventManager()
-	{
-		return eventManager;
-	}
-
+	/**
+	 * Checks if any of the scripts is running.
+	 * 
+	 * @return True if any of the scripts is running
+	 */
 	public boolean areScriptsRunning()
 	{
 		for (final Script script : scripts.values())
@@ -222,5 +272,15 @@ public class ScriptManager
 		}
 
 		return false;
+	}
+	
+	/**
+	 * Gets the scripts value.
+	 *  
+	 * @return The scripts value
+	 */
+	public Map<File, Script> getScripts()
+	{
+		return scripts;
 	}
 }
